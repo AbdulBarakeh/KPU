512e5fa44018a385653b6cbd6b5072b7a9e3499b
Abdul <abdulbarakeh98@gmail.com>
2020-08-17 12:52:02 +0200

    slides and code

--- KPUEKSAMEN/Spoergsmaal_TRE/DotNETComponent/CPRValidaterShared/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
+++ KPUEKSAMEN/Spoergsmaal_TRE/DotNETComponent/CPRValidaterShared/obj/Debug/netcoreapp3.1/MainWindow.g.i.cs
@@ -101,7 +101,7 @@ namespace CPRValidaterShared {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/CPRValidaterShared;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/CPRValidaterShared;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
